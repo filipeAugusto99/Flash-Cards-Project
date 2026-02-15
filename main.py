3c5d0235453ceb3c2c0a0c132dcc5efeb612f63b
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-15 13:23:29 -0300

    Saving Progress

--- main.py
+++ main.py
@@ -3,31 +3,50 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-SHORT_BREAK_SEG = 3
 
 flip_timer = None
 current_card = {}
+to_learn = {}
+
+# Create New Flash Cards
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
 def next_card():
     global flip_timer, current_card
-    window.after_cancel(flip_timer) if flip_timer else None
+
+    if flip_timer is not None:
+        window.after_cancel(flip_timer)
+
     current_card = random.choice(to_learn)
+
     canvas.itemconfig(title_text, text="French")
     canvas.itemconfig(word_text, text=current_card["French"])
-    flip_timer = window.after(3000, flip_card)
     canvas.itemconfig(create_image, image=card_front_img)
+
+    flip_timer = window.after(3000, flip_card)
 
 
 def flip_card():
     canvas.itemconfig(title_text, text="English")
     canvas.itemconfig(word_text, text=current_card["English"])
     canvas.itemconfig(create_image, image=card_back_img)
-    
-    
 
-# Create New Flash Cards
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient='records')
+
+def is_known():
+    global current_card
+    to_learn.remove(current_card)
+
+    data_save = pandas.DataFrame(to_learn)
+    data_save.to_csv("data/words_to_learn.csv", index=False)
+
+    next_card()
 
 
 # UI SETUP
@@ -53,14 +72,13 @@ word_text = canvas.create_text(
 
 canvas.grid(column=0, row=0, columnspan=2)
 
-
 # Create Buttons
-    # RIght Button
+# RIght Button
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=next_card)
+right_button = Button(image=right_image, highlightthickness=0, command=is_known)
 right_button.grid(column=1, row=1)
 
-    # Wrong Button
+# Wrong Button
 wrong_image = PhotoImage(file="images/wrong.png")
 wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
